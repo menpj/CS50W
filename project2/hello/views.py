a802c36cd0f813813b997884abe595d152746acf
menpj <3994084+menpj@users.noreply.github.com>
2024-03-27 00:40:29 +0530

    .\project2

---
+++ project2/hello/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request,"hello/index.html")
+    #return HttpResponse("Hello, Noah!")
+    #return HttpResponse("<h1 style=\"color:blue\">Hello, world!</h1>")
+def noah(request):
+    return HttpResponse("Hello, Noah!")
+
+def david(request):
+    return HttpResponse("Hello, David!")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {"name": name.capitalize()})
+
+   
