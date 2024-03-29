076feb7104983d9937cbd9b075226e062a0c3795
menpj <3994084+menpj@users.noreply.github.com>
2024-03-29 13:34:21 +0530

    ./

--- project2/tasks/views.py
+++ project2/tasks/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 tasks = ["foo","bar","baz"]
+
+class NewTaskForm(forms.Form):
+    task=forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority",min_value=1,max_value=10)
 
 def index(request):
     return render(request,"tasks/index.html",{
@@ -9,4 +15,17 @@ def index(request):
     })
 
 def add(request):
-    return render(request, "tasks/add.html")
+
+    if request.method=="POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            
+            return render(request,"tasks/add.html",{
+                 "form":form
+            })
+    
+    return render(request,"tasks/add.html" ,{"form":NewTaskForm()} )
