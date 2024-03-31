16acfdee09fb70bd73ccc9c8d8c0affd3e426882
menpj <3994084+menpj@users.noreply.github.com>
2024-03-31 23:34:00 +0530

    ./

--- project2/tasks/views.py
+++ project2/tasks/views.py
@@ -10,8 +10,13 @@ class NewTaskForm(forms.Form):
     priority = forms.IntegerField(label="Priority",min_value=1,max_value=10)
 
 def index(request):
+
+    if "tasks" not in request.session:
+
+        request.session["tasks"]=[]
+
     return render(request,"tasks/index.html",{
-        "tasks":tasks
+        "tasks":request.session["tasks"]
     })
 
 def add(request):
@@ -20,7 +25,8 @@ def add(request):
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            request.session["tasks"]+=[task]
+            #tasks.append(task)
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
             
