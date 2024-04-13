38fc1db2cef251332da2e75e8b236bb0b4cb1d7a
menpj <3994084+menpj@users.noreply.github.com>
2024-04-13 15:14:45 +0530

    error message added in missing entry in wiki project

--- project3/web50/projects/2020/x/wiki/encyclopedia/urls.py
+++ project3/web50/projects/2020/x/wiki/encyclopedia/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name= "encyclopedia"
+
 urlpatterns = [
     path("", views.index, name="index"),
     #path("content",views.content,name="content"),
     path("wiki",views.wiki,name="wiki"),
-    path("wiki/<str:name>",views.wiki,name="wiki")
+    path("wiki/<str:name>",views.wiki,name="wiki"),
+    path("wikiSearch",views.wikiSearch,name="wikiSearch"),
+    path("newPage",views.newPage,name="newPage")
+    
 ]
--- project3/web50/projects/2020/x/wiki/encyclopedia/views.py
+++ project3/web50/projects/2020/x/wiki/encyclopedia/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 from . import util
 
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django import forms
+
 import markdown2
+
+import re
 
 
 def index(request):
@@ -14,9 +20,6 @@ def index(request):
 def wiki(request,name):
     #name=request.GET.get('name')
     entryBody=util.get_entry(name)
-
-    
-
 
 
     if entryBody:
@@ -31,3 +34,60 @@ def wiki(request,name):
             "content": None
         })
     
+
+def wikiSearch(request):
+
+    if request.method=="POST":
+        name=request.POST['q']
+        entryBody=util.get_entry(name)
+        if entryBody:
+            entryBody= markdown2.markdown(entryBody)
+            file1 = open(f"encyclopedia/templates/wiki/{name}.html","w")
+            
+            file1.write(entryBody)
+            file1.close()
+            #return render(request, f"wiki/{name}.html")
+            return HttpResponseRedirect(f"wiki/{name}")
+        else:
+            entryList=util.list_entries()
+            matchEntryList = []
+            for entry in entryList:
+                match= re.search(name,entry,re.I)
+                if match:
+                    matchEntryList.append(entry)
+                else:
+                    pass
+                    #matchEntryList.append(entry)
+
+            #print(matchEntryList)
+            if matchEntryList:
+                return render(request, "encyclopedia/search.html", {
+                    "entries": matchEntryList
+                })
+            else:
+
+                return render(request, "encyclopedia/content.html", {
+                    "content": None
+            })    
+    
+
+class NewTaskForm(forms.Form):
+    title=forms.CharField(label="New Page Title",help_text="New Page Title",empty_value="Page Title",max_length=100)
+    body=forms.CharField(label="Page Body",widget=forms.Textarea)
+    #priority = forms.IntegerField(label="Priority",min_value=1,max_value=10)
+
+def newPage(request):
+    if request.method=="POST":
+        form=NewTaskForm(request.POST)
+        if form.is_valid():
+            title=form.cleaned_data["title"]
+            body=form.cleaned_data["body"]
+            print(title)
+            print(body)
+            return HttpResponseRedirect(reverse("encyclopedia:index"))
+        else:
+            return render(request,"encyclopedia/newpage.html",{
+                "form":form
+            })
+    return render(request, "encyclopedia/newpage.html",{"form": NewTaskForm()}) 
+
