a5d1c62bf3b476cf7c92ac1925bbf53e2198bc37
menpj <3994084+menpj@users.noreply.github.com>
2024-04-17 13:19:20 +0530

    some message

--- project3/web50/projects/2020/x/wiki/encyclopedia/views.py
+++ project3/web50/projects/2020/x/wiki/encyclopedia/views.py
@@ -149,26 +149,40 @@ class EditForm(forms.Form):
         super().__init__()
         print("test")
         self.titleName=name
+        print("test 2")
         self.fields['title'].initial=name
-
+        print("test 4")
         entryBody=util.get_entry(name)
-
+        print("test 5")
         self.fields['body'].initial= entryBody
+        
         #self.title=forms.CharField(label="Page Title",initial="test"   ,max_length=100)
         #self.body=forms.CharField(label="Page Body",widget=forms.Textarea,initial="sample body")
     title=forms.CharField(label="Page Title" ,max_length=100)
     body=forms.CharField(label="Page Body",widget=forms.Textarea)
+    print("test 3")
     
 
 def editPage(request,name):
-    #print("test")
-    #print(name)
-    #return render(request, "encyclopedia/greet.html", {"name": name.capitalize()})
-    #if request.method=="POST":
-        
-    #edit=EditForm.initial(name="different one")
+    
+
+    if request.method=="POST":
+        print("post test 1")
+        form = EditForm(request.POST)
+        print("post test 2")
+        if form.is_valid():
+            title=form.cleaned_data["title"]
+            body=form.cleaned_data["body"]
+            print(title)
+            print(body)
+            return HttpResponseRedirect(f"wiki/{title}")
+        else:
+            return render(request,"encyclopedia/editpage.html",{
+                "form":form
+            })
+            
 
 
     
     return render(request,"encyclopedia/editpage.html",{"name": name,"form": EditForm(name)})
-    #return render(request,"encyclopedia/editpage.html",{"name": name,"form": edit()})
+    
