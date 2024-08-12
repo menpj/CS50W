7ab29e45d5af8b0aa15487dc80973731c8427f9f
menpj <3994084+menpj@users.noreply.github.com>
2024-08-12 16:49:27 +0530

    commit after completing course contents

---
+++ folder/wiki/wiki/encyclopedia/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def content(request):
+    name=request.GET.get('name')
+    return render(request, "encyclopedia/content.html", {
+        "content": util.get_entry(name)
+    })
+
